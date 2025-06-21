0414234cf12c82f9ab39813db4931270985ab467
shashidhar078 <nagunurishashidhar@gmail.com>
2025-06-21 16:02:42 +0530

    resolved buttons

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const img=document.querySelectorAll(".header-slider ul img");
-const pre=document.querySelectorAll(".ctrl-prev");
-const next=document.querySelectorAll(".ctrl-next");
+const pre=document.querySelector(".ctrl-prev");
+const next=document.querySelector(".ctrl-next");
+
+next.addEventListener("click",()=>{
+    changingSlide();
+})
+
+
+
+
+function changingSlide()
+{
+    for(let i=0;i<img.length;i++)
+    {
+        img[i].style.display="none";
+    }
+}
