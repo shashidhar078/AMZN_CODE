213c50a36eccc63a47ec66ea80300fb9d2048037
shashidhar078 <nagunurishashidhar@gmail.com>
2025-06-21 19:18:48 +0530

    done with arrow buttons

--- app.js
+++ app.js
@@ -2,17 +2,37 @@ const img=document.querySelectorAll(".header-slider ul img");
 const pre=document.querySelector(".ctrl-prev");
 const next=document.querySelector(".ctrl-next");
 
-next.addEventListener("click",()=>{
-    changingSlide();
-})
+let n=0;
 
-
-
-
-function changingSlide()
+function changingSlide(n)
 {
     for(let i=0;i<img.length;i++)
     {
         img[i].style.display="none";
     }
+    img[n].style.display="block";
 }
+
+next.addEventListener("click",()=>{
+    if(n<img.length-1)
+    {
+        n++;
+    }
+    else 
+    {
+        n=0;
+    }
+    changingSlide(n);
+})
+
+pre.addEventListener("click",()=>{
+    if(n>0)
+    {
+        n--;
+    }
+    else 
+    {
+        n=img.length-1;
+    }
+    changingSlide(n);
+})
